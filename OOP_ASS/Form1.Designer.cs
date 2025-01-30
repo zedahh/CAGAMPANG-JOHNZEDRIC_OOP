45052d16113a1324e4b471e7055e6e1d1e9ba955
User <User@LAPTOP-AH38G6CL>
2025-01-30 15:53:53 +0800

    Add project files.

---
+++ OOP_ASS/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace OOP_ASS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rectangle = new System.Windows.Forms.Button();
+            this.circle = new System.Windows.Forms.Button();
+            this.triangle = new System.Windows.Forms.Button();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.txtBase = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rectangle
+            // 
+            this.rectangle.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.rectangle.Location = new System.Drawing.Point(118, 144);
+            this.rectangle.Name = "rectangle";
+            this.rectangle.Size = new System.Drawing.Size(97, 53);
+            this.rectangle.TabIndex = 0;
+            this.rectangle.Text = "CALCULATE";
+            this.rectangle.UseVisualStyleBackColor = false;
+            this.rectangle.Click += new System.EventHandler(this.rectangle_Click);
+            // 
+            // circle
+            // 
+            this.circle.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.circle.Location = new System.Drawing.Point(304, 142);
+            this.circle.Name = "circle";
+            this.circle.Size = new System.Drawing.Size(97, 54);
+            this.circle.TabIndex = 0;
+            this.circle.Text = "CALCULATE";
+            this.circle.UseVisualStyleBackColor = false;
+            this.circle.Click += new System.EventHandler(this.circle_Click);
+            // 
+            // triangle
+            // 
+            this.triangle.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.triangle.Location = new System.Drawing.Point(501, 143);
+            this.triangle.Name = "triangle";
+            this.triangle.Size = new System.Drawing.Size(97, 54);
+            this.triangle.TabIndex = 0;
+            this.triangle.Text = "CALCULATE";
+            this.triangle.UseVisualStyleBackColor = false;
+            this.triangle.Click += new System.EventHandler(this.triangle_Click);
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(133, 48);
+            this.txtLength.Multiline = true;
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(71, 32);
+            this.txtLength.TabIndex = 1;
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(133, 93);
+            this.txtWidth.Multiline = true;
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(71, 32);
+            this.txtWidth.TabIndex = 1;
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(315, 47);
+            this.txtRadius.Multiline = true;
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(70, 33);
+            this.txtRadius.TabIndex = 1;
+            this.txtRadius.TextChanged += new System.EventHandler(this.txtRadius_TextChanged);
+            // 
+            // txtBase
+            // 
+            this.txtBase.Location = new System.Drawing.Point(516, 51);
+            this.txtBase.Multiline = true;
+            this.txtBase.Name = "txtBase";
+            this.txtBase.Size = new System.Drawing.Size(70, 33);
+            this.txtBase.TabIndex = 1;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(516, 96);
+            this.txtHeight.Multiline = true;
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(70, 33);
+            this.txtHeight.TabIndex = 1;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(210, 225);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(116, 38);
+            this.txtResult.TabIndex = 1;
+            this.txtResult.Text = "texboxResult";
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.FormattingEnabled = true;
+            this.listBoxResult.Location = new System.Drawing.Point(365, 225);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(120, 95);
+            this.listBoxResult.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Length";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(269, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Radius";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(472, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Height";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(92, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Width";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(472, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Base";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(132, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "RECTANGLE";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(326, 22);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "CIRCLE";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(513, 22);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(61, 13);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "TRIANGLE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBoxResult);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtBase);
+            this.Controls.Add(this.txtRadius);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.triangle);
+            this.Controls.Add(this.circle);
+            this.Controls.Add(this.rectangle);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button rectangle;
+        private System.Windows.Forms.Button circle;
+        private System.Windows.Forms.Button triangle;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.TextBox txtBase;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.ListBox listBoxResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ OOP_ASS/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_ASS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rectangle_Click(object sender, EventArgs e)
+        {
+            double length = Convert.ToDouble(txtLength.Text);
+            double width = Convert.ToDouble(txtWidth.Text);
+            double area = CalculateArea(length, width);
+            DisplayResult($"Rectangle: {area}");
+        }
+
+        private void circle_Click(object sender, EventArgs e)
+        {
+            double radius = Convert.ToDouble(txtRadius.Text);
+            double area = CalculateArea(radius);
+            DisplayResult($"Circle: {area}");
+        }
+
+        private void triangle_Click(object sender, EventArgs e)
+        {
+            double baseLength = Convert.ToDouble(txtBase.Text);
+            double height = Convert.ToDouble(txtHeight.Text);
+            double area = CalculateArea(baseLength, height, true);
+            DisplayResult($"Triangle: {area}");
+        }
+        private double CalculateArea(double length, double width)
+        {
+            return length * width;
+        }
+        private double CalculateArea(double radius)
+        {
+            double pi = 3.14159;
+            return pi * radius * radius;
+        }
+        private double CalculateArea(double baseLength, double height, bool isTriangle)
+        {
+            return 0.5 * baseLength * height;
+        }
+        private void DisplayResult(string result)
+        {
+            listBoxResult.Items.Add(result);
+            txtResult.Text = result;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtRadius_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
